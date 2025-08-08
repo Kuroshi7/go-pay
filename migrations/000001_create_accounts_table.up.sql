5953aef3cc5e7b498af07bad930656491fbc3de9
Victor Soffi <victorsoffi@gmail.com>
2025-08-08 15:52:30 -0300

    [fix] typos and docker compose

--- migrations/000001_create_accounts_table.up.sql
+++ migrations/000001_create_accounts_table.up.sql
@@ -1,4 +1,4 @@
-CREAE TABLE IF NOT EXISTS accounts (
+CREATE TABLE IF NOT EXISTS accounts (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
