dc79fea43696d864e51e3cdcdf9dd170282653f7
Kuroshi7 <victorsoffi@gmail.com>
2025-09-25 19:41:12 -0300

    fix: cors

--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require (
 	github.com/lib/pq v1.10.9
 
 )
+
+require github.com/go-chi/cors v1.2.2 // indirect
--- internal/web/server/server.go
+++ internal/web/server/server.go
@@ -4,42 +4,50 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/kuroshi7/go-pay/internal/service"
 	"github.com/kuroshi7/go-pay/internal/web/handlers"
-	"github.com/kuroshi7/go-pay/internal/web/middleware"
+	authMiddleware "github.com/kuroshi7/go-pay/internal/web/middleware"
 )
 
 type Server struct {
 	//Server fields
-	router *chi.Mux
-	server *http.Server
+	router         *chi.Mux
+	server         *http.Server
 	accountService *service.AccountService
 	invoiceService *service.InvoiceService
-	port string
-
+	port           string
 }
 
 func NewServer(accountService *service.AccountService, invoiceService *service.InvoiceService, port string) *Server {
 	return &Server{
-		router: chi.NewRouter(),
+		router:         chi.NewRouter(),
 		accountService: accountService,
 		invoiceService: invoiceService,
-		port: port,
+		port:           port,
 	}
 }
 
-func (s *Server) ConfigureRoutes(){
+func (s *Server) ConfigureRoutes() {
 	acocuntHandler := handlers.NewAccountHandler(s.accountService)
 	invoiceHandler := handlers.NewInvoiceHandler(s.invoiceService)
-	authMiddleware := middleware.NewAuthMiddleware(s.accountService)
+	authMidd := authMiddleware.NewAuthMiddleware(s.accountService)
 
-
+	// Configure CORS
+	s.router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-API-Key"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	s.router.Post("/accounts", acocuntHandler.Create)
 	s.router.Get("/accounts", acocuntHandler.Get)
-	
+
 	s.router.Group(func(r chi.Router) {
-		r.Use(authMiddleware.Authenticate)
+		r.Use(authMidd.Authenticate)
 		r.Post("/invoices", invoiceHandler.Create)
 		r.Get("/invoices/{id}", invoiceHandler.GetByID)
 		r.Get("/invoices", invoiceHandler.ListByAccount)
@@ -52,7 +60,6 @@ func (s *Server) Start() error {
 	s.server = &http.Server{
 		Addr:    ":" + s.port,
 		Handler: s.router,
-
-}
+	}
 	return s.server.ListenAndServe()
 }
